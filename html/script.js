57031b92cddb1f64668d9a0c107d2dfb408a82e4
sava hastoci <savah022025@gmail.com>
2025-02-24 11:07:46 +0100

    commit

---
+++ html/script.js
@@ -0,0 +1,106 @@
+console.log ("hello word")
+
+var  x = 5 // vi bruker ikke var lengre
+const  name: = " sava"
+
+
+
+let firstname  = " emil " // string
+let age = 30 integer: number 
+let exampleFloat = 32.2 //Float
+let frombergen = true // boolean
+let eksampleArrey = = [" Mazda, "opel", "saab", "mistubish",]
+let myobject  = {
+   first name: "torleif",
+   lastname: "leiknes",
+   age: 67 ,
+   email: " torleif@leiknes.no"
+}
+
+console. log (exampleArray)
+console.log(eksamlpeFloat)
+
+
+let x = 11
+let y = 4
+
+x++
+console .log (x)
+let z = x + y
+console.log(z)
+
+//operators
+
+let a = 10
+let b = 15
+
+console. log (a + b)
+console.log (a - b )
+console.log (a / b)
+console.log (a * b)
+console.log (a & b)
+
+let X = 10
+let y = "10"
+
+console. log ( x > y)// større en
+console. log (x < y) // mindre en
+console.log (x == y) // sjenker om det er sammaen
+console.log (x === y) //stykly equal to
+console.log (x  ! =y) // its not equal to
+
+console. log (x  < y)// mindre eller link
+console.log (x > y)// større eller link
+
+// logical operators og / eller operatørs
+
+let ervoksen  = true
+let barneskole = false
+
+console .log ( erVoksen &&  ! bartneskole)
+
+//conditisionals 
+
+const age = 17
+if ( age > 18 {
+    console.log ("Velcomen inn")
+} else {
+
+}console .log (" get lost")
+
+let  name = " tjollof"
+let numberoffCats = 7
+
+
+if  (numberoffCats) <= 20 && firstName === " Magda"
+  console.log (" yopu have to many cats")
+  else if ( numberoffCats <= 20) && firstname ===)
+    else{
+console.log (" hvor er kristin o katene")
+
+
+}
+let user = {
+    user name:  "dj sjur"
+    user age: 49,
+    user City: " sandefjor"
+
+
+}
+
+if ( username === " PER "){
+    Cconsole.log (" Velcome Per")
+    
+}else if ( userage <= 18) {
+    console.log (" you are not old enaugh")
+    {
+        else if
+    }
+}
+
+
+
+
+
+
+
